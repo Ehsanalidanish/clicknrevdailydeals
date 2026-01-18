b89e32c37f1f5587362f4bc21034bbdd0b4526ee
Ehsanalidanish <ehsanali106@gmail.com>
2026-01-17 19:11:33 -0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, ClickNRev Daily Deals!</h1>
+      <p>Your site is working!</p>
+    </div>
+  );
+};
+
+export default App;
